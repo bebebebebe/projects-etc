9f0f82f42528d3ea8b75b7020fc570565ac23009
Be <bebirchall@gmail.com>
2013-06-27 17:41:38 -0400

    comments, including web interface

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+
+  def new
+    @article = Article.find(params[:id])
+    @comment = @article.comments.new
+  end
+
+  def create
+    @comment = Comment.new(params[:comment])
+    @comment.article_id = params[:article_id]
+    @comment.save
+    
+    redirect_to article_path(@comment.article)
+  end
+
+end
